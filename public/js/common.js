42ca938ae8c159196522a096b15148e52029fdf6
niudongchao <1456831664@qq.com>
2017-09-21 11:47:12 +0800

    模板引擎重构登录功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define (['jquery','cookie'],function($){
+define (['jquery','cookie'],function($,template){
  //NProgress.start();
  //NProgress.done();
 
@@ -50,7 +50,14 @@ define (['jquery','cookie'],function($){
      //判断一下，信息是否存在如果不存在就不会跳转
       loginInfo = loginInfo && JSON.parse(loginInfo); 
       //设置用户的头像信息
-      // $('.aside .profile img').attr('src',loginInfo.tc_avatar);
-      // $('.aside .profile h4').html(loginInfo.tc_name);
+     //$('.aside .profile img').attr('src',loginInfo.tc_avatar);
+     //$('.aside .profile h4').html(loginInfo.tc_name);
+
+   var tpl = ' <div class="avatar img-circle">
+                <img src="{{tc_avatar}}">
+            </div><h4>{{tc_name}}</h4>';
+       
+   var html = template.render(tpl,loginInfo);
+    $('.aside .profile').html(html);
 });
 	
