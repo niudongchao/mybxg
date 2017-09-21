2f25128f7dddfc6f3144c56cdcb22f9ec76e450e
niudongchao <1456831664@qq.com>
2017-09-21 20:34:52 +0800

    最终版本模块引擎重构登录功能和部分讲师列表展示功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define (['jquery','cookie'],function($,template){
+define (['jquery','template','cookie'],function($,template){
  //NProgress.start();
  //NProgress.done();
 
@@ -53,10 +53,9 @@ define (['jquery','cookie'],function($,template){
      //$('.aside .profile img').attr('src',loginInfo.tc_avatar);
      //$('.aside .profile h4').html(loginInfo.tc_name);
 
-   var tpl = ' <div class="avatar img-circle">
-                <img src="{{tc_avatar}}">
-            </div><h4>{{tc_name}}</h4>';
-       
+   var tpl = '<div class="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
+      
+  console.log(loginInfo) 
    var html = template.render(tpl,loginInfo);
     $('.aside .profile').html(html);
 });
