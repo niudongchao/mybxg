7aa06816c1286b399af44086819bfbbe009eeabe
niudongchao <1456831664@qq.com>
2017-09-22 11:28:06 +0800

    完成启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,4 +1,4 @@
-define(['jquery','template'],function($,template){
+define(['jquery','template','bootstrap'],function($,template){
 	//调用接口获取所有的讲师数据
 	$.ajax({
 		type :'get',
@@ -13,7 +13,7 @@ define(['jquery','template'],function($,template){
 
 			 //启用登录注销功能  绑定事件
 			 $('.eod').click(function(){
-			 	var that = this;
+			        	var that = this;
                      // console.log(123);
                      var td = $(this).closest('td');
                      // console.log(td);
@@ -37,6 +37,26 @@ define(['jquery','template'],function($,template){
                     });
 
 			 });
+
+
+       //查看讲师
+         $('.preview').click(function(){
+           var td = $(this).closest('td');
+           // console.log(td);
+           var tcId = td.attr('data-tcId');
+           $.ajax({
+            type:'get',
+            url:'/api/teacher/view',
+            data:{tc_id:tcId},
+            dataType:'json',
+            success:function(data){
+              // console.log(data);
+              var html = template('modalTpl',data.result);
+              $('#modalInfo').html(html);
+              $('#teacherModal').modal();
+            }
+           });
+         });
 		}
 	});     
 });
