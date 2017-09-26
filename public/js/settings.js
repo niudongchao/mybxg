2b515f5ca49467283ee07af39ee02aaeb02e8ee8
niudongchao <1456831664@qq.com>
2017-09-26 09:39:36 +0800

    添加文本功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','uploadify','region'],function($,template){
+define(['jquery','template','ckeditor','uploadify','region','datepicker','language'],function($,template,CKEDITOR){
    //调用接口，获取个人信息
    $.ajax({
    	type:'get',
@@ -27,6 +27,15 @@ define(['jquery','template','uploadify','region'],function($,template){
     $('#pcd').region({
       url:'/public/assets/jquery-region/region.json'
     });
-   	}
+
+    // 处理富文本
+    CKEDITOR.replace('editor',{
+   
+toolbarGroups : [
+    { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+   ]
+     });
+	}
    });
 });
