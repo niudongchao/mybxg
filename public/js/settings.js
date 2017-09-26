8edc64932ad8bb62e09bca8dbbb37d9d93d0707b
niudongchao <1456831664@qq.com>
2017-09-26 11:04:26 +0800

    完成个人中心表单提交功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,10 +1,11 @@
-define(['jquery','template','ckeditor','uploadify','region','datepicker','language'],function($,template,CKEDITOR){
+define(['jquery','template','ckeditor','uploadify','region','datepicker','language','validate','form'],function($,template,CKEDITOR){
    //调用接口，获取个人信息
    $.ajax({
    	type:'get',
    	url:'/api/teacher/profile',
    	dataType:'json',
    	success:function(data){
+      console.log(data);
     //解析数据，渲染页面
     var html = template('settingsTpl',data.result);
     $('#settingsInfo').html(html);
@@ -31,11 +32,47 @@ define(['jquery','template','ckeditor','uploadify','region','datepicker','langua
     // 处理富文本
     CKEDITOR.replace('editor',{
    
-toolbarGroups : [
-    { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-   ]
+    toolbarGroups : [
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+       ]
      });
+   
+
+
+    $('#settingsForm').validate({
+        sendForm : false,
+        valid : function(){
+          // 获取家乡信息
+          var p = $('#p').find('option:selected').text();
+          var c = $('#c').find('option:selected').text();
+          var d = $('#d').find('option:selected').text();
+          var hometown = p + '|' + c + '|' + d;
+          // 同步富文本内容
+          for(var instance in CKEDITOR.instances){
+            CKEDITOR.instances[instance].updateElement();
+          }
+          // 提交表单
+          $(this).ajaxSubmit({
+            type : 'post',
+            url : '/api/teacher/modify',
+            data : {tc_hometown:hometown},
+            dataType : 'json',
+            success : function(data){
+              if(data.code == 200){
+                // 修改成功之后重新刷新当前页码
+                // location.reload();
+              }
+            }
+          });
+        }
+      });
+
+
+
+
+
+
 	}
    });
 });
